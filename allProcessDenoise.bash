9a20113ad83d8d7eac6ed7e0f2b6b37a3d9dd715
trevormoser <moser.trevor@gmail.com>
2024-03-25 10:05:31 -0700

    Added script for serialEM montage convertion to .tif

--- allProcessDenoise.bash
+++ allProcessDenoise.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/motionCorr.bash
+bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/motionCorr.bash
 bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/topazDenoise.bash
 bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/Restack_SerialEM_denoise.bash
 bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/IMOD_Auto_denoise.bash
