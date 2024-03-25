b4e8e2a672c07665b30cf495d813d54557565293
trevormoser <moser.trevor@gmail.com>
2024-03-25 12:53:30 -0700

    Updated allProcess.bash and removed allProcessDenoise.bash

--- allProcess.bash
+++ allProcess.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/motionCorr.bash
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/Restack_SerialEM.bash
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/IMOD_Auto.bash
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/topazDenoise.bash
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/reconstructFromNoisy.bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+bash $SCRIPT_DIR/motionCorr.bash
+bash $Script_DIR/topazdenoise.bash
+bash $SCRIPT_DIR/Restack_SerialEM.bash
+bash $SCRIPT_DIR/IMOD_Auto.bash
 
--- allProcessDenoise.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/motionCorr.bash
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/topazDenoise.bash
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/Restack_SerialEM_denoise.bash
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/IMOD_Auto_denoise.bash
-bash /home/mose831/ImageProcessingScripts/cryoem_processing/tomoProcess/reconstructFromDenoise.bash
-
