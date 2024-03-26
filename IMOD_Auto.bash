4225cf638d1c2f4db270b100f48d777af5af9e52
trevormoser <moser.trevor@gmail.com>
2024-03-26 12:30:31 -0700

    Added ability to check .skip file for IMOD_Auto.bash script

--- IMOD_Auto.bash
+++ IMOD_Auto.bash
@@ -21,11 +21,22 @@ do
 
 	if [[ "$topazModel" == "NA" ]]
 	then
-       		target=AlignedStacks/$tiltName
+       		target=$dir/$tiltName
 	else
-		target=AlignedStacks/$tiltName/$topazDir
+		target=$dir/$tiltName/$topazDir
 	fi
+#Checks for a .skip file containing a comma separated list of tilts to skip, if found, appends the cryoDirective.adoc with skip list	
+	if [ -f $dir/$tiltName/${tiltName}.skip ] ;
+	then 
+		echo "Skipping views in $tiltName..."
+		skipList=$(cat $dir/$tiltName/${tiltName}.skip)
+		sed -i "s/^setupset.copyarg.skip =.*/setupset.copyarg.skip =${skipList}/" cryoDirective.adoc
 
+	else
+		echo "Not skipping any tilts for $tiltName..."
+		sed -i "s/^setupset.copyarg.skip =.*/setupset.copyarg.skip =/" cryoDirective.adoc
+	fi
+#Checks if an imod directory exists already exists for this tilt series, if it doesn't, run batchruntomo
 	if [ -d $target/$tiltName ] ;
 	then
 		echo "IMOD has already been run in "$target/$tiltName/", skipping..."
--- motionCorr.bash
+++ motionCorr.bash
@@ -13,46 +13,45 @@ then
 	echo "Motion correction has already been performed, skipping..."
 else
 
-GainRefDM4=*.dm4
-dm2mrc $GainRefDM4 GainRef.mrc
-newstack -in GainRef.mrc -bi 1 -ou GainRef.mrc
-GainRef=GainRef.mrc
+	GainRefDM4=*.dm4
+	dm2mrc $GainRefDM4 GainRef.mrc
+	newstack -in GainRef.mrc -bi 1 -ou GainRef.mrc
+	GainRef=GainRef.mrc
 
-mkdir $dir
+	mkdir $dir
 
 #Check file suffix in frames folder and determine if raw frames are .tifs or .mrcs
 #Set suf and motcor (either -InTiff or -InMrc) variables depending on which is found
-for file in frames/*; do
-	if [ "${file##*.}" == "mrc" ]
-	then 
-		suf=mrc
-		motCor=-InMrc
-	elif [ "${file##*.}" == "tif" ]
-	then 
-		suf=tif
-		motCor=-InTiff
-	fi
-done
+	for file in frames/*; do
+		if [ "${file##*.}" == "mrc" ]
+		then 
+			suf=mrc
+			motCor=-InMrc
+		elif [ "${file##*.}" == "tif" ]
+		then 
+			suf=tif
+			motCor=-InTiff
+		fi
+	done
 
 #Run motioncorrection on each image found in frames directory
 #Get basename from original images for output name and image name to call motion correction on
-for image in frames/*.$suf; do
-base_name=`basename ${image} .$suf`
-imageName=${base_name}".$suf"
-echo $base_name
+	for image in frames/*.$suf; do
+		base_name=`basename ${image} .$suf`
+		imageName=${base_name}".$suf"
+		echo $base_name
 
-/msc/krios/motioncor2/MotionCor2_1.3.2-Cuda102 "$motCor" frames/$imageName \
--OutMrc SumFrames/${base_name}_sumavg.mrc \
--Patch 5 5 20 \
--GPU 0 1 \
--Gain $GainRef \
--FlipGain 1 \
--Iter 10 \
--Tol 0.5 \
--FtBin $motCorBin \
+		/msc/krios/motioncor2/MotionCor2_1.3.2-Cuda102 "$motCor" frames/$imageName \
+		-OutMrc SumFrames/${base_name}_sumavg.mrc \
+		-Patch 5 5 20 \
+		-GPU 0 1 \
+		-Gain $GainRef \
+		-FlipGain 1 \
+		-Iter 10 \
+		-Tol 0.5 \
+		-FtBin $motCorBin \
 
-done
-
+	done
 fi
 
 end_time=$(date +%s)
